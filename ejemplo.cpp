b43a6a3a9077c11fbbb5fe3c23915952ffa19559
JoseBernalRodriguez <jobernalr@unal.edu.co>
2021-02-26 12:45:04 -0500

    severo cambio

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-
+#include <cstdlib>
 int main(int argc, char **argv)
 {
-    std::cout << "hola mundo "<< "\n";
+    int a = std::atoi(argv[1]);
+    std::cout << "hola mundo "<< 2*a << "\n";
     return 0;
 
 
